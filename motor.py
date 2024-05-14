b9143fc363d86326e43e42f27862e0b50e160a37
David Miles <davidmiles1925@gmail.com>
2024-05-14 00:04:05 -0500

    get motor spinning

--- motor.py
+++ motor.py
@@ -1,11 +1,8 @@
-from gpiozero import OutputDevice
+import RPi.GPIO as GPIO
 from time import sleep
 
-# Define GPIO pins connected to the motor driver IN1 - IN4
-IN1 = OutputDevice(17)
-IN2 = OutputDevice(18)
-IN3 = OutputDevice(27)
-IN4 = OutputDevice(22)
+# The pins we will use to drive the motor on the Raspberry Pi
+MOTOR_PIN_ARRAY = [17,18,27,22]
 
 # Define the stepper motor sequence
 step_sequence = [
@@ -20,26 +17,55 @@ step_sequence = [
 ]
 
 # Define the motor speed and number of steps for a full rotation
-step_delay = 0.005  # Delay between steps in seconds
-steps_per_revolution = 4096  # Steps required for a full rotation
+step_delay = 0.00293  # Delay between steps in seconds (0.00293 will result in 5 RPM)
+#step_delay = 1
+
+steps_per_revolution = 4096  # Steps required for a full rotation\
+
+motor_step_counter = 0
+
+def set_up_pins():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    for pin in MOTOR_PIN_ARRAY:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+        print(f"Pin {pin} set as output")
+
 
 def step(direction, steps):
-    for _ in range(steps):
-        for pin in range(4):
-            IN1.value = step_sequence[direction][0]
-            IN2.value = step_sequence[direction][1]
-            IN3.value = step_sequence[direction][2]
-            IN4.value = step_sequence[direction][3]
-            sleep(step_delay)
+    global motor_step_counter
 
-# Rotate the motor 360 degrees clockwise
-step(1, steps_per_revolution)
+    print(f"step: {direction} {steps}")
+    for i in range(steps):
+        for pin in range(len(MOTOR_PIN_ARRAY)):
+            GPIO.output(MOTOR_PIN_ARRAY[pin], step_sequence[motor_step_counter][pin])
+            print(step_sequence[motor_step_counter][pin])
+        if direction == True:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        elif direction == False:
+            motor_step_counter = (motor_step_counter - 1) % 8
+            print(f"*****{motor_step_counter}*****")
+        else:
+            print("That didn't work")
+        sleep(step_delay)
 
-# Rotate the motor 360 degrees counterclockwise
-step(-1, steps_per_revolution)
 
-# Cleanup GPIO
-IN1.close()
-IN2.close()
-IN3.close()
-IN4.close()
+if __name__ == "__main__":
+    try:
+        set_up_pins()
+        turn_counter = 0
+
+        # Rotate the motor 360 degrees clockwise
+        for num in range(5):
+            step(False, steps_per_revolution)
+            turn_counter = turn_counter + 1
+            print(f"Completed {turn_counter} revolution")
+
+    except KeyboardInterrupt:
+        print("Program was closed using ctrl+c")
+        GPIO.cleanup()
+
+    except Exception as e:
+        print("Something went wrong.")
