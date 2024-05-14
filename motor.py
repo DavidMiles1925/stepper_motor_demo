0f290e505913770eb94ebcff642ac6b7b690e95f
David Miles <davidmiles1925@gmail.com>
2024-05-14 00:46:50 -0500

    update comments

--- motor.py
+++ motor.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+number_of_turns = 5
+
 # The pins we will use to drive the motor on the Raspberry Pi
-MOTOR_PIN_ARRAY = [17,18,27,22]
+MOTOR_PIN_ARRAY = [23,22,27,17]
 
 # Define the stepper motor sequence
 step_sequence = [
@@ -16,12 +18,23 @@ step_sequence = [
     [0, 0, 0, 1]
 ]
 
-# Define the motor speed and number of steps for a full rotation
-step_delay = 0.00293  # Delay between steps in seconds (0.00293 will result in 5 RPM)
-#step_delay = 1
 
-steps_per_revolution = 4096  # Steps required for a full rotation\
+# Delay between steps in seconds
+step_delay = 0.0009765625 
 
+# STEP DELAY VALUES:
+# DELAY = (60/RPM) / 4096
+#   1 RPM: 0.0146484375
+#   2 RPM: 0.00732421875
+#   5 RPM: 0.0029296875
+#  10 RPM: 0.00146484375
+#  15 RPM: 0.0009765625
+
+
+# Steps required for a full rotation\
+steps_per_revolution = 4096  
+
+# This number will assist in outputting our sequence
 motor_step_counter = 0
 
 def set_up_pins():
@@ -41,12 +54,10 @@ def step(direction, steps):
     for i in range(steps):
         for pin in range(len(MOTOR_PIN_ARRAY)):
             GPIO.output(MOTOR_PIN_ARRAY[pin], step_sequence[motor_step_counter][pin])
-            print(step_sequence[motor_step_counter][pin])
         if direction == True:
             motor_step_counter = (motor_step_counter + 1) % 8
         elif direction == False:
             motor_step_counter = (motor_step_counter - 1) % 8
-            print(f"*****{motor_step_counter}*****")
         else:
             print("That didn't work")
         sleep(step_delay)
@@ -58,8 +69,8 @@ if __name__ == "__main__":
         turn_counter = 0
 
         # Rotate the motor 360 degrees clockwise
-        for num in range(5):
-            step(False, steps_per_revolution)
+        for num in range(number_of_turns):
+            step(True, steps_per_revolution)
             turn_counter = turn_counter + 1
             print(f"Completed {turn_counter} revolution")
 
